fac0441181b2b1ff14ce7067367346e9128e51c3
imxsquash <coussotelwen@gmail.com>
2026-06-06 17:02:57 +0200

    feat(routes): wire forum index and thread detail routes

--- src/routes/_protected/forum/index.tsx
+++ src/routes/_protected/forum/index.tsx
@@ -1,5 +1,6 @@
+import { Forum } from '@/components/forum/Forum';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_protected/forum/')({
-  component: () => null,
+  component: Forum,
 });
