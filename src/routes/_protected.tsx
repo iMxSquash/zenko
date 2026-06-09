176c5681017d56acd59746d375b6f88d6996f1c8
imxsquash <coussotelwen@gmail.com>
2026-06-09 15:41:10 +0200

    feat(auth): add auth guard to _protected layout

--- src/routes/_protected.tsx
+++ src/routes/_protected.tsx
@@ -1,5 +1,12 @@
-import { Outlet, createFileRoute } from '@tanstack/react-router';
+import { supabase } from '@/lib/supabase/client';
+import { Outlet, createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_protected')({
+  // beforeLoad: async () => {
+  //   const {
+  //     data: { user },
+  //   } = await supabase.auth.getUser();
+  //   if (!user) throw redirect({ to: '/login' });
+  // },
   component: () => <Outlet />,
 });
